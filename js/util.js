e99954ca485a65e4d184a9481147f013962255c2
DorTeri <117217009+DorTeri@users.noreply.github.com>
2023-01-25 17:09:53 +0200

    first commit

---
+++ js/util.js
@@ -0,0 +1,50 @@
+'use strict'
+
+function createMat(ROWS, COLS) {
+    var mat = []
+    for (var i = 0; i < ROWS; i++) {
+        var row = []
+        for (var j = 0; j < COLS; j++) {
+            row.push('')
+        }
+        mat.push(row)
+    }
+    return mat
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function getTime() {
+    return new Date().toString().split(' ')[4];
+}
+
+function renderCell(location, value) {
+    // Select the elCell and set the value
+    const elCell = document.querySelector(`.cell-${location.i}-${location.j}`)
+    elCell.innerText = value
+}
+
+function getEmptyLocation(board) {
+    var emptyPoss = []
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] !== MINE) emptyPoss.push({ i, j })
+        }
+    }
+    return emptyPoss
+}
+
+function copyMat(mat) {
+    var newMat = []
+    for (var i = 0; i < mat.length; i++) {
+        newMat[i] = []
+        for (var j = 0; j < mat[0].length; j++) {
+            newMat[i][j] = mat[i][j]
+        }
+    }
+    return newMat
+}
